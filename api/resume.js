ef4a0c85798ddeabb50eef8df9a1541a62189993
gabriellamsen332-oss <gabriellamsen332@gmail.com>
2025-12-08 16:38:57 +0800

    .

--- api/resume.js
+++ api/resume.js
@@ -1,6 +1,6 @@
 const PDFDocument = require('pdfkit');
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   try {
     const doc = new PDFDocument({
       size: 'A4',
@@ -65,4 +65,4 @@ module.exports = async (req, res) => {
     console.error('PDF generation error:', error);
     res.status(500).json({ error: 'Failed to generate PDF' });
   }
-};
+}
